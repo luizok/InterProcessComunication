e45705d0aca0c95d9f3e6c5c13cc334a590c6bea
Luiz Felipe <luizfelipesmaciel@gmail.com>
2018-07-04 21:22:10 -0300

    Added package 'child_process' and establishing comunication between NodeJS and Python

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,20 @@
 #! /usr/bin/python3
 
 import zmq
+import sys
+import json
+from pprint import pprint
 
 if __name__ == '__main__':
 	#TODO: Some process over the data received by JS Server
-	print('Iae men')
+	# Why arg[0] != python3 ????
+	print("Current args in python: " + str(sys.argv))
+	if len(sys.argv) < 2:
+		print('Usage: python3 <filename> <jsonStr>');
+		sys.exit(0);
+	else:
+		obj = json.loads(sys.argv[1])
+		for k, v in obj.items():
+			print("{} = {}".format(k, v))
+
+		sys.exit(1);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const
 	cluster = require('cluster'),
 	express = require('express'),
 	app = express(),
+	spawn = require('child_process').spawn,
 	CPU_NUM = require('os').cpus().length;
 
 
@@ -36,19 +37,32 @@ if(cluster.isMaster) {
 	});
 
 	app.get('/test', (req, res) => {
-		let str = 'Era essa a peca que voce queria ?\n ' + JSON.stringify(req.query);
-		let request = zmq.socket('req').connect('tcp://127.0.0.1:6666');
+		//let str = 'Era essa a peca que voce queria ?\n ' + JSON.stringify(req.query);
+		//let request = zmq.socket('req').connect('tcp://127.0.0.1:6666');
 
-		request.send(JSON.stringify(req.query));
+		//request.send(JSON.stringify(req.query));
 
-		request.on('message', function(data) {
-			let obj = JSON.parse(data);
-			console.log(
-				'\033[1;33m' + obj.pid + ' did his job working on json \n' 
-				   + JSON.stringify(obj.processed_data) + '\033[m'
-			);
-			console.log();
-			res.send(obj);
+		//request.on('message', function(data) {
+		//	let obj = JSON.parse(data);
+		//	console.log(
+		//		'\033[1;33m' + obj.pid + ' did his job working on json \n' 
+		//		   + JSON.stringify(obj.processed_data) + '\033[m'
+		//	);
+		//	console.log();
+		//	res.send(obj);
+		//});
+		let obj = JSON.stringify(req.query);
+
+		pyProcess = spawn('python3', ['data_processing.py', obj]);
+
+		pyProcess.stdout.on('data', (data) => { console.log(data.toString('utf8')); });
+		pyProcess.stderr.on('data', (data) => { console.log(data.toString('utf8')); });
+		pyProcess.on('close', (code) => {
+			console.log("Python process ended with code " + code);
+			if(code == 0)
+				res.send('Houve um erro durante o processamento');
+			else
+				res.send('Tudo sussa, meu parca :D');
 		});
 	});
 
